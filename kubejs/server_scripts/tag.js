0eac436f72d78f9dd5e68d9071bbb71425923653
THIRAPHATCHAKON <thiraphatchakon@gmail.com>
2026-06-12 23:42:38 +0700

    fix repo

---
+++ kubejs/server_scripts/tag.js
@@ -0,0 +1,163 @@
+ServerEvents.tags('item', event => {
+
+        event.add('kubejs:hunter_armor_helmet', [
+        'survival_instinct:desert_hunter_helmet',
+        'survival_instinct:green_hunter_helmet',
+        'survival_instinct:black_hunter_helmet',
+    ])
+
+    event.add('kubejs:recluit_armor_helmet', [
+        'survival_instinct:desert_recluit_armor_helmet',
+        'survival_instinct:green_recluit_armor_helmet',
+        'survival_instinct:black_recluit_armor_helmet',
+    ])
+
+    event.add('kubejs:recluit_armor_chestplate', [
+        'survival_instinct:desert_recluit_armor_chestplate',
+        'survival_instinct:green_recluit_armor_chestplate',
+        'survival_instinct:black_recluit_armor_chestplate',
+    ])
+
+    event.add('kubejs:recluit_armor_leggings', [
+        'survival_instinct:desert_recluit_armor_leggings',
+        'survival_instinct:green_recluit_armor_leggings',
+        'survival_instinct:black_recluit_armor_leggings',
+    ])
+
+    event.add('kubejs:recluit_armor_boots', [
+        'survival_instinct:desert_recluit_armor_boots',
+        'survival_instinct:green_recluit_armor_boots',
+        'survival_instinct:black_recluit_armor_boots',
+    ])
+
+    event.add('kubejs:rockie_armor_helmet', [
+        'survival_instinct:desert_rockie_armor_helmet',
+        'survival_instinct:green_rockie_armor_helmet',
+        'survival_instinct:black_rockie_armor_helmet',
+    ])
+
+    event.add('kubejs:rockie_armor_chestplate', [
+        'survival_instinct:desert_rockie_armor_chestplate',
+        'survival_instinct:green_rockie_armor_chestplate',
+        'survival_instinct:black_rockie_armor_chestplate',
+    ])
+
+    event.add('kubejs:rockie_armor_leggings', [
+        'survival_instinct:desert_rockie_armor_leggings',
+        'survival_instinct:green_rockie_armor_leggings',
+        'survival_instinct:black_rockie_armor_leggings',
+    ])
+
+    event.add('kubejs:rockie_armor_boots', [
+        'survival_instinct:desert_rockie_armor_boots',
+        'survival_instinct:green_rockie_armor_boots',
+        'survival_instinct:black_rockie_armor_boots',
+    ])
+
+    event.add('kubejs:faz_roleplay_boots', [
+        'faz_roleplay_plus:red_shoes_boots',
+        'faz_roleplay_plus:blue_shoes_boots',
+        'faz_roleplay_plus:green_shoes_boots',
+    ])
+
+    event.add('kubejs:guillie_armor_helmet', [
+        'survival_instinct:guillie_helmet',
+        'survival_instinct:artic_guillie_helmet',
+        'survival_instinct:spruce_guillie_helmet',
+    ])
+
+    event.add('kubejs:guillie_armor_chestplate', [
+        'survival_instinct:guillie_chestplate',
+        'survival_instinct:artic_guillie_chestplate',
+        'survival_instinct:spruce_guillie_chestplate',
+    ])
+
+    event.add('kubejs:guillie_armor_leggings', [
+        'survival_instinct:guillie_leggings',
+        'survival_instinct:artic_guillie_leggings',
+        'survival_instinct:spruce_guillie_armor_leggings',
+    ])
+
+    event.add('kubejs:guillie_armor_boots', [
+        'survival_instinct:guillie_boots',
+        'survival_instinct:artic_guillie_boots',
+        'survival_instinct:spruce_guillie_boots',
+    ])
+
+    event.add('kubejs:juggernaut_armor_helmet', [
+        'survival_instinct:black_juggernaut_helmet',
+        'survival_instinct:desert_juggernaut_helmet',
+        'survival_instinct:green_juggernaut_helmet',
+    ])
+
+    event.add('kubejs:juggernaut_armor_chestplate', [
+        'survival_instinct:black_juggernaut_chestplate',
+        'survival_instinct:desert_juggernaut_chestplate',
+        'survival_instinct:green_juggernaut_chestplate',
+    ])
+
+    event.add('kubejs:juggernaut_armor_leggings', [
+        'survival_instinct:black_juggernaut_leggings',
+        'survival_instinct:desert_juggernaut_leggings',
+        'survival_instinct:green_juggernaut_leggings',
+    ])
+
+    event.add('kubejs:juggernaut_armor_boots', [
+        'survival_instinct:black_juggernaut_boots',
+        'survival_instinct:desert_juggernaut_boots',
+        'survival_instinct:green_juggernaut_boots',
+    ])
+
+    event.add('kubejs:recon_armor_helmet', [
+        'omegarecon:crim_helmet_helmet',
+        'omegarecon:airforce_helmet_helmet',
+        'omegarecon:panzercrew_helmet_helmet',
+        'omegarecon:blackout_helmet_helmet',
+        'omegarecon:ghillie_helmet_helmet',
+        'omegarecon:arctic_helmet_helmet',
+        'omegarecon:viper_helmet_helmet',
+        'omegarecon:redalert_helmet_helmet',
+        'omegarecon:kevlar_helmet_helmet',
+        'omegarecon:secretservice_helmet_helmet',
+    ])
+
+    event.add('kubejs:recon_armor_chestplate', [
+        'omegarecon:crim_vest_chestplate',
+        'omegarecon:airforce_vest_chestplate',
+        'omegarecon:panzercrew_vest_chestplate',
+        'omegarecon:blackout_vest_chestplate',
+        'omegarecon:ghillie_vest_chestplate',
+        'omegarecon:arctic_vest_chestplate',
+        'omegarecon:viper_vest_chestplate',
+        'omegarecon:redalert_vest_chestplate',
+        'omegarecon:kevlar_vest_chestplate',
+        'omegarecon:secretservice_vest_chestplate',
+    ])
+
+    event.add('kubejs:recon_armor_leggings', [
+        'omegarecon:crim_chinguard_leggings',
+        'omegarecon:airforce_pants_leggings',
+        'omegarecon:panzercrew_pants_leggings',
+        'omegarecon:blackout_pants_leggings',
+        'omegarecon:ghillie_pants_leggings',
+        'omegarecon:arctic_pants_leggings',
+        'omegarecon:viper_pants_leggings',
+        'omegarecon:redalert_pants_leggings',
+        'omegarecon:kevlar_chinguard_leggings',
+        'omegarecon:secretservice_pants_leggings',
+    ])
+
+    event.add('kubejs:recon_armor_boots', [
+        'omegarecon:crimson_boots_boots',
+        'omegarecon:airforce_boots_boots',
+        'omegarecon:panzercrew_boots_boots',
+        'omegarecon:blackout_boots_boots',
+        'omegarecon:ghillie_boots_boots',
+        'omegarecon:arctic_boots_boots',
+        'omegarecon:viper_boots_boots',
+        'omegarecon:redalert_boots_boots',
+        'omegarecon:combat_boots_boots',
+        'omegarecon:secretservice_boots_boots',
+    ])
+
+})
